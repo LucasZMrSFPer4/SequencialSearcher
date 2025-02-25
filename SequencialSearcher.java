4adafa0488999dc95bd4018a278be29e85a682b8
LucasZMrSFPer4 <lucas.zhang26@student.tamdistrict.org>
2025-02-25 09:39:06 -0800

    git commit

--- SequencialSearcher.java
+++ SequencialSearcher.java
@@ -1,4 +1,19 @@
 public class SequencialSearcher
 {
+    private int[] array = new int[10];
+
+    public SequencialSearcher()
+    {
+        for (int i = 0; i < 10; i++) 
+        {
+            array[i] = (int) (Math.random() * 10 + 1);
+        }
+    }
     
+    public static int find(int[] array, int value)
+    {
+        return value;
+    }
+
+
 }
